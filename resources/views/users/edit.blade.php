66938c53e5254d40a027e688f68d6b5e11b920a6
TimothyBeldeniza <bsdtimothy@gmail.com>
2021-07-09 00:12:03 +0800

    Permissions Ui

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,11 +2,12 @@
     <style>
         .container 
         {
-            max-width:1200px;
+            /* max-width:1200px; */
             margin: 0 auto;
-            display: flex;
-            flex-wrap: wrap;
-    
+            /* display: flex; */
+            border: 5px solid black;
+            width: 100%;
+            text-align: center;
         }
         /* .container div 
         {
@@ -15,49 +16,59 @@
         .container div label
         {
             cursor: pointer;
+            
+
         }
         .container div label input[type=checkbox]
         {
+
             display: none;
+            
         }
 
         .container div label span
         {
-            /* position: relative; */
-            /* display: inline-block; */
-            background: gray;
-            /* padding: 15px 30px; */
+            
+            /* position: relative;
+            display: inline-block; */
+            background: white;
+            padding: 5px 5px;
             color: #555;
             /* text-shadow: 0 1px 4px rgba(0,0,0,.5); */
             border-radius: 20px;
             transition: 0.5s;
             user-select:none;
             overflow: hidden;
+            border: 2px solid rgb(56, 193, 114);
         }
         .container div label span:before
         {
-            content: '';
-            /* position: absolute; */
+            /* content: ''; */
+            position: absolute;
             top: 0;
             left: 0;
             width: 100%; 
             height: 50%;
-            /* background: rgba(184, 179, 179, 0.1); */
+            background: rgba(184, 179, 179, 0.1);
         }
         .container div label input[type=checkbox]:checked ~ span
         {
-            background: rgb(115, 179, 86);
+            background: rgb(56, 193, 114);
+            color: white;
+
         }
+
     </style>
 </head>
 
 
 <body>
+<div class="container">
     <form method="POST" action="{{ route('users.update', $user->id) }}" >
         @csrf
         @method('PUT')
         
-        <div class="form-group row">
+        {{-- <div class="form-group row">
             <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
             
             <div class="col-md-6">
@@ -71,33 +82,39 @@
                 </select>
             </div>
             
-        </div>
+        </div> --}}
 
-        <div class="form-group row">
-            <label for="permissions" class="col-md-4 col-form-label text-md-right">{{ __('Permissions') }}</label>
+        {{-- <div class="form-group row"> --}}
+            {{-- <label for="permissions" class="col-md-4 col-form-label text-md-right">{{ __('Permissions') }}</label> --}}
             
-            <div class="col-md-6">
+            {{-- <div class="col-md-6"> --}}
                 
                
                     {{-- <option @if ($userRole[0] == 'Admin') return selected @endif value="Admin">Admin</option>
                     <option @if ($userRole[0] == 'User') return selected @endif value="User">User</option> --}}
 
-                    @foreach ( $permissions as $permission )
+                    
                         {{-- <label class="checkbox-inline"><input type="checkbox" value="{{ $permission->name}}"><span class="badge">{{ $permission->name}}</span></label> --}}
-                        <div class="container">
+                      
+
                             <div>
-                                <label class="badge badge-success">
+                                @foreach ( $permissions as $permission )
+                                <label>
+                                    
                                     <input type="checkbox">
                                     <span>{{ $permission->name}}</span>
+                                    
                                 </label>
+                                @endforeach
                             </div>
-                        </div>
-                       
-                    @endforeach
 
-            </div>
+
+                       
+                    
+
+            {{-- </div>
             
-        </div>
+        </div> --}}
 
         <div class="form-group row mb-0">
             <div class="col-md-6 offset-md-4 ">
@@ -109,6 +126,7 @@
 
         
     </form>
+</div>
 </body>
 
                     
