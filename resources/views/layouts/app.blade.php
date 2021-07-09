6a8d6f897abe325a51dafc1e6bab751d774c4172
TimothyBeldeniza <bsdtimothy@gmail.com>
2021-07-09 23:45:03 +0800

    App

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,13 +21,6 @@
 
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
-    <style>
-
-        body {
-            background-color: #fff5e5
-        }
-
-    </style>
 </head>
 <body>
     <div id="app">
